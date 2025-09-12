073d7ac45ea27dfd83155ee6ffceefa9d27e7687
teaongsi <angdembe.teaongsi@gmail.com>
2025-09-12 07:23:24 +0545

    Update tasker dashboard
    Add search

--- frontend/src/components/TaskerDashboard.jsx
+++ frontend/src/components/TaskerDashboard.jsx
@@ -1,5 +1,41 @@
+import { useEffect, useMemo, useState } from "react";
+import SearchIcon from '@mui/icons-material/Search';
+import api from "../api/axios";
+
 export default function TaskerDashboard() {
+    const [search, setSearch] = useState("");
+
     return (
-        <div>This is tasker dashboard</div>
-    )
+        <div className="taskerDashboard">
+            <div className="searchRow">
+                <div className="bigSearch">
+                    <input 
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        placeholder="Search tasks..." 
+                    />
+                    <div className="searchIcon"><SearchIcon /></div>
+                </div>
+                <div className="categoryRow">
+                    <button className="categoryChip">Categories</button>
+                    <button className="categoryChip">Categories</button>
+                    <button className="categoryChip">Categories</button>
+                    <button className="categoryChip">Categories</button>
+                    <button className="categoryChip">Categories</button>
+                    <button className="categoryChip">Categories</button>
+                    <button className="categoryChip">Categories</button>
+                    <button className="categoryChip">Categories</button>
+                    <button className="categoryChip">Categories</button>
+                    <button className="categoryChip">Categories</button>
+                    <button className="categoryChip">Categories</button>
+                    <button className="categoryChip">Categories</button>
+                    <button className="categoryChip">Categories</button>
+                </div>
+            </div>
+            <h3>Available Tasks</h3>
+
+            <div className="tasksGrid">
+            </div>
+        </div>
+    );
 }
